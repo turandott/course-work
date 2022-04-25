1a2b93501f927aa1ec2640c322433299824b5d41
turandott <greyvilka@gmail.com>
2022-04-25 18:41:05 +0300

    add app helper method and change the list partial for properties

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def profile_picture account
+    thumb = account.image.attached? ? url_for(account.image) : "default_avatar.png"
+    image_tag(thumb, width: 150, class: "profile_pic img-circle")
+  end
 end
