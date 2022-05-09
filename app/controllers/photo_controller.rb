5d53bc71e7441a7e2426fd6455bb4535069e91a0
turandott <greyvilka@gmail.com>
2022-05-09 23:41:51 +0300

    update

--- app/controllers/photo_controller.rb
+++
@@ -1,7 +0,0 @@
-class PhotosController < ApplicationController
-  def update
-    # params[:avatar] contains an ActionDispatch::Http::UploadedFile object
-    Current.propert.photo.attach(params.require(:photo))
-    redirect_to Current.propert
-  end
-  end
